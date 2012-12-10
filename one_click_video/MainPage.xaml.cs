a2434e784257b425a154782839603509443965b2
Florian Kaiser <florian.kaiser@floyd-ug.de>
2012-12-10 16:26:44 +0100

    camera working

--- one_click_video/MainPage.xaml.cs
+++ one_click_video/MainPage.xaml.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using one_click_video.Resources;
+using Microsoft.Devices;
+using Windows.Phone.Media.Capture;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Threading;
+using Microsoft.Phone;
 
 namespace one_click_video
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private AudioVideoCaptureDevice _dev;
+        //private Windows.Foundation.Size _max_s;
+        //private int[] _arr;
+        //private WriteableBitmap _oB;
+
+
         // Konstruktor
         public MainPage()
         {
@@ -20,7 +32,72 @@ namespace one_click_video
 
             // Beispielcode zur Lokalisierung der ApplicationBar
             //BuildLocalizedApplicationBar();
+
+            Loaded += MainPage_Loaded;
         }
+
+        async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Windows.Foundation.Size resolution = new Windows.Foundation.Size(640, 480);
+            _dev = await AudioVideoCaptureDevice.OpenAsync(CameraSensorLocation.Back, resolution);
+
+            //_dev.SetProperty(KnownCameraGeneralProperties.SpecifiedCaptureOrientation, 90);
+            //_dev.SetProperty(KnownCameraGeneralProperties.EncodeWithOrientation, 90);
+
+            _dev.SetProperty(KnownCameraAudioVideoProperties.VideoTorchPower, AudioVideoCaptureDevice.GetSupportedPropertyRange(CameraSensorLocation.Back, KnownCameraAudioVideoProperties.VideoTorchPower).Min);
+            _dev.SetProperty(KnownCameraAudioVideoProperties.VideoTorchMode, VideoTorchMode.On);
+
+            Windows.Foundation.Size actualResolution = _dev.PreviewResolution;
+
+            CameraStreamSource source = new CameraStreamSource(_dev, actualResolution);
+            MyCameraMediaElement.SetSource(source);
+
+            //IReadOnlyList<Windows.Foundation.Size>  cap_res = AudioVideoCaptureDevice.GetAvailableCaptureResolutions(CameraSensorLocation.Back);
+            //_max_s = new Windows.Foundation.Size(0, 0);
+            
+            //foreach (Windows.Foundation.Size s in cap_res)
+            //{
+            //    if (s.Height * s.Width > _max_s.Height * _max_s.Width)
+            //    {
+            //        _max_s = s;
+            //    }
+            //}
+
+            //_arr = new int[(int)(_max_s.Height * _max_s.Width)];
+            //_oB = new WriteableBitmap(int)_max_s.Width, (int)_max_s.Height);
+
+            //IAsyncOperation<AudioVideoCaptureDevice> dev_async_op = AudioVideoCaptureDevice.OpenAsync(CameraSensorLocation.Back, _max_s);
+            //dev_async_op.Completed = (IAsyncOperation<AudioVideoCaptureDevice> dev, Windows.Foundation.AsyncStatus status) =>
+            //    {
+            //        _dev = dev.GetResults();
+            //        _dev.PreviewFrameAvailable += _dev_PreviewFrameAvailable;
+            //    };
+
+            
+
+                //OpenAsync(CameraSensorLocation.Back, new Windows.Foundation.Size(10,10));
+
+        }
+
+        //private void _dev_PreviewFrameAvailable(ICameraCaptureDevice sender, object args)
+        //{
+        //    sender.GetPreviewBufferArgb(_arr);
+
+        //    this.Dispatcher.BeginInvoke(() =>
+        //    {
+        //        for (int i = 0; i < _arr.Length; ++i)
+        //        {
+        //            _oB.Pixels[i] = _arr[i];
+        //        }
+
+        //        this.img.Source = _oB;
+        //    }
+        //    );
+           
+
+        //    //sender.GetPreviewBufferArgb
+        //}
+
 
         // Beispielcode zur Erstellung einer lokalisierten ApplicationBar
         //private void BuildLocalizedApplicationBar()
