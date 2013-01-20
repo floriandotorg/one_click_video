815cc1071cb0a14125496413d5d04b9d7323efb1
Florian Kaiser <florian.kaiser@floyd-ug.de>
2013-01-20 17:07:47 +0100

    MainPage -> RecordingPage

--- one_click_video/MainPage.xaml.cs
+++ one_click_video/MainPage.xaml.cs
@@ -24,101 +24,16 @@ namespace one_click_video
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        private AudioVideoCaptureDevice _dev;
-        private VideoBrush _videoBrush;
-        private IRandomAccessStream _sst;
-        private string _path;
-
         // Konstruktor
         public MainPage()
         {
             InitializeComponent();
 
             // Beispielcode zur Lokalisierung der ApplicationBar
-            //BuildLocalizedApplicationBar();
-
-            OrientationChanged += MainPage_OrientationChanged;
+            //BuildLocalizedApplicationBar();d;
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-
-            //CameraCaptureTask cameraCaptureTask = new CameraCaptureTask();
-            //cameraCaptureTask.Show();
-
-            ActivateCamera();
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            if (_dev != null)
-            {
-                _dev.Dispose();
-                _dev = null;
-            }
-
-            base.OnNavigatedFrom(e);
-        }
-
-        private void MainPage_OrientationChanged(object sender, OrientationChangedEventArgs e)
-        {
-            if (this.Orientation == PageOrientation.LandscapeLeft)
-            {
-                _videoBrush.RelativeTransform = new RotateTransform() { CenterX = 0.5, CenterY = 0.5, Angle = 90 - _dev.SensorRotationInDegrees };
-            }
-            else if (this.Orientation == PageOrientation.LandscapeRight)
-            {
-                _videoBrush.RelativeTransform = new RotateTransform() { CenterX = 0.5, CenterY = 0.5, Angle = 90 + _dev.SensorRotationInDegrees };
-            }
-            else if(this.Orientation == PageOrientation.PortraitUp)
-            {
-                _videoBrush.RelativeTransform = new RotateTransform() { CenterX = 0.5, CenterY = 0.5, Angle = _dev.SensorRotationInDegrees };
-            }
-        }
-
-        async void ActivateCamera()
-        {
-            if (_dev == null)
-            {
-                if (AudioVideoCaptureDevice.AvailableSensorLocations.Contains(CameraSensorLocation.Back))
-                {
-                    _dev = await AudioVideoCaptureDevice.OpenAsync(CameraSensorLocation.Back, AudioVideoCaptureDevice.GetAvailableCaptureResolutions(CameraSensorLocation.Back).First());
-
-                    _videoBrush = new VideoBrush();
-
-                    _videoBrush.SetSource(_dev);
-                    MainPage_OrientationChanged(null, null);
-
-                    this.videoRect.Fill = _videoBrush;
-
-                    StorageFolder localFolder = ApplicationData.Current.LocalFolder;
-                    StorageFile storageFile = await localFolder.CreateFileAsync("CameraMovie.mp4", CreationCollisionOption.ReplaceExisting);
-                    _path = storageFile.Path;
-
-                    _sst = await storageFile.OpenAsync(FileAccessMode.ReadWrite);
-                    await _dev.StartRecordingToStreamAsync(_sst);
-                }
-            }
-        }
-
-        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
-        {
-        	if (_dev != null)
-            {
-                _dev.StopRecordingAsync().Completed = (IAsyncAction action, Windows.Foundation.AsyncStatus status) =>
-                    {
-                        _dev.Dispose();
-                        _dev = null;
-
-
-                        new MediaPlayerLauncher()
-                        {
-                            Media = new Uri(_path, UriKind.Relative),
-                        }.Show();
-                    };
-            }
-        }
+        
 
         // Beispielcode zur Erstellung einer lokalisierten ApplicationBar
         //private void BuildLocalizedApplicationBar()
---
+++ one_click_video/RecordingPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Devices;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Phone.Media.Capture;
+using Windows.Foundation;
+
+namespace one_click_video
+{
+    public partial class RecordingPage : PhoneApplicationPage
+    {
+        private AudioVideoCaptureDevice _dev;
+        private VideoBrush _videoBrush;
+        private IRandomAccessStream _sst;
+        private string _path;
+
+        public RecordingPage()
+        {
+            InitializeComponent();
+
+            OrientationChanged += RecordingPage_OrientationChanged;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            //CameraCaptureTask cameraCaptureTask = new CameraCaptureTask();
+            //cameraCaptureTask.Show();
+
+            ActivateCamera();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            if (_dev != null)
+            {
+                _dev.Dispose();
+                _dev = null;
+            }
+
+            base.OnNavigatedFrom(e);
+        }
+
+        private void RecordingPage_OrientationChanged(object sender, OrientationChangedEventArgs e)
+        {
+            if (this.Orientation == PageOrientation.LandscapeLeft)
+            {
+                _videoBrush.RelativeTransform = new RotateTransform() { CenterX = 0.5, CenterY = 0.5, Angle = 90 - _dev.SensorRotationInDegrees };
+            }
+            else if (this.Orientation == PageOrientation.LandscapeRight)
+            {
+                _videoBrush.RelativeTransform = new RotateTransform() { CenterX = 0.5, CenterY = 0.5, Angle = 90 + _dev.SensorRotationInDegrees };
+            }
+            else if (this.Orientation == PageOrientation.PortraitUp)
+            {
+                _videoBrush.RelativeTransform = new RotateTransform() { CenterX = 0.5, CenterY = 0.5, Angle = _dev.SensorRotationInDegrees };
+            }
+        }
+
+        async void ActivateCamera()
+        {
+            if (_dev == null)
+            {
+                if (AudioVideoCaptureDevice.AvailableSensorLocations.Contains(CameraSensorLocation.Back))
+                {
+                    _dev = await AudioVideoCaptureDevice.OpenAsync(CameraSensorLocation.Back, AudioVideoCaptureDevice.GetAvailableCaptureResolutions(CameraSensorLocation.Back).First());
+
+                    _videoBrush = new VideoBrush();
+
+                    _videoBrush.SetSource(_dev);
+                    RecordingPage_OrientationChanged(null, null);
+
+                    this.videoRect.Fill = _videoBrush;
+
+                    StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+                    StorageFile storageFile = await localFolder.CreateFileAsync("CameraMovie.mp4", CreationCollisionOption.ReplaceExisting);
+                    _path = storageFile.Path;
+
+                    _sst = await storageFile.OpenAsync(FileAccessMode.ReadWrite);
+                    await _dev.StartRecordingToStreamAsync(_sst);
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_dev != null)
+            {
+                _dev.StopRecordingAsync().Completed = (IAsyncAction action, Windows.Foundation.AsyncStatus status) =>
+                {
+                    _dev.Dispose();
+                    _dev = null;
+                };
+            }
+        }
+    }
+}
